9c166339c2c4bdae0d61cbdd2db50a57998b74ac
Tony Sherman <30444383+tsh3rman@users.noreply.github.com>
2018-12-08 00:52:41 -0500

    Delete app_test.py

--- app_test.py
+++
@@ -1,7 +0,0 @@
-from podcast import download_podcast_library, get_episode_data
-
-num_of_episodes = 0
-podcast_url = 'http://feeds.everythingisalive.com/everythingisalive'
-# podcast_url = 'hello'
-data = get_episode_data(podcast_url, num_of_episodes)
-library = download_podcast_library(data)
